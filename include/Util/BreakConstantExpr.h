8ef26adf7d500bec03d241030fc1157c7757a85f
Yulei Sui <rockysui@gmail.com>
2016-05-05 12:38:57 +1000

    Update SVF license

--- include/Util/BreakConstantExpr.h
+++ include/Util/BreakConstantExpr.h
@@ -3,7 +3,7 @@
 //                          The SAFECode Compiler
 //
 // This file was developed by the LLVM research group and is distributed under
-// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// GPLv3 Open Source License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
 //
