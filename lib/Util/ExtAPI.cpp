b2797157654b3f538c869523a3721dfceddac8cd
Mohamad Barbar <mbarbar@runbox.com>
2018-03-21 12:29:00 +1100

    Add map from ExtAPI string to enum.

--- lib/Util/ExtAPI.cpp
+++ lib/Util/ExtAPI.cpp
@@ -834,6 +834,45 @@ static const ei_pair ei_pairs[]= {
  *  hasmntopt - returns arg0->mnt_opts
  */
 
+llvm::StringMap<ExtAPI::extf_t> stringToExtAPI;
+
+static void fillExtAPIStringMap() {
+    stringToExtAPI["EFT_NOOP"]             = ExtAPI::EFT_NOOP;
+    stringToExtAPI["EFT_ALLOC"]            = ExtAPI::EFT_ALLOC;
+    stringToExtAPI["EFT_REALLOC"]          = ExtAPI::EFT_REALLOC;
+    stringToExtAPI["EFT_FREE"]             = ExtAPI::EFT_FREE;
+    stringToExtAPI["EFT_NOSTRUCT_ALLOC"]   = ExtAPI::EFT_NOSTRUCT_ALLOC;
+    stringToExtAPI["EFT_STAT"]             = ExtAPI::EFT_STAT;
+    stringToExtAPI["EFT_STAT2"]            = ExtAPI::EFT_STAT2;
+    stringToExtAPI["EFT_L_A0"]             = ExtAPI::EFT_L_A0;
+    stringToExtAPI["EFT_L_A1"]             = ExtAPI::EFT_L_A1;
+    stringToExtAPI["EFT_L_A2"]             = ExtAPI::EFT_L_A2;
+    stringToExtAPI["EFT_L_A8"]             = ExtAPI::EFT_L_A8;
+    stringToExtAPI["EFT_L_A0__A0R_A1R"]    = ExtAPI::EFT_L_A0__A0R_A1R;
+    stringToExtAPI["EFT_A1R_A0R"]          = ExtAPI::EFT_A1R_A0R;
+    stringToExtAPI["EFT_A3R_A1R_NS"]       = ExtAPI::EFT_A3R_A1R_NS;
+    stringToExtAPI["EFT_A1R_A0"]           = ExtAPI::EFT_A1R_A0;
+    stringToExtAPI["EFT_A2R_A1"]           = ExtAPI::EFT_A2R_A1;
+    stringToExtAPI["EFT_A4R_A1"]           = ExtAPI::EFT_A4R_A1;
+    stringToExtAPI["EFT_L_A0__A2R_A0"]     = ExtAPI::EFT_L_A0__A2R_A0;
+    stringToExtAPI["EFT_A0R_NEW"]          = ExtAPI::EFT_A0R_NEW;
+    stringToExtAPI["EFT_A1R_NEW"]          = ExtAPI::EFT_A1R_NEW;
+    stringToExtAPI["EFT_A2R_NEW"]          = ExtAPI::EFT_A2R_NEW;
+    stringToExtAPI["EFT_A4R_NEW"]          = ExtAPI::EFT_A4R_NEW;
+    stringToExtAPI["EFT_A11R_NEW"]         = ExtAPI::EFT_A11R_NEW;
+    stringToExtAPI["EFT_STD_RB_TREE_INSERT_AND_REBALANCE"]
+        = ExtAPI::EFT_STD_RB_TREE_INSERT_AND_REBALANCE;
+    stringToExtAPI["EFT_STD_RB_TREE_INCREMENT"]
+        = ExtAPI::EFT_STD_RB_TREE_INCREMENT;
+    stringToExtAPI["EFT_STD_LIST_HOOK"]    = ExtAPI::EFT_STD_LIST_HOOK;
+    stringToExtAPI["CPP_EFT_A0R_A1"]       = ExtAPI::CPP_EFT_A0R_A1;
+    stringToExtAPI["CPP_EFT_A0R_A1R"]      = ExtAPI::CPP_EFT_A0R_A1R;
+    stringToExtAPI["CPP_EFT_A1R"]          = ExtAPI::CPP_EFT_A1R;
+    stringToExtAPI["EFT_CXA_BEGIN_CATCH"]  = ExtAPI::EFT_CXA_BEGIN_CATCH;
+    stringToExtAPI["CPP_EFT_DYNAMIC_CAST"] = ExtAPI::CPP_EFT_DYNAMIC_CAST;
+    stringToExtAPI["EFT_OTHER"]            = ExtAPI::EFT_OTHER;
+}
+
 
 void ExtAPI::init() {
     set<extf_t> t_seen;
