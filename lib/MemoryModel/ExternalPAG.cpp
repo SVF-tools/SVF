e2b1ab0f9fe42b08eff54312d9f96752339febb8
Mohamad Barbar <mbarbar@runbox.com>
2018-09-28 18:27:54 +1000

    Add ExternalPAG.cpp.

---
+++ lib/MemoryModel/ExternalPAG.cpp
@@ -0,0 +1,248 @@
+//===- ExternalPAG.cpp -- Program assignment graph ---------------------------//
+
+/*
+ * ExternalPAG.cpp
+ *
+ *  Created on: Sep 22, 2018
+ *      Author: Mohamad Barbar
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "MemoryModel/PAG.h"
+#include "MemoryModel/ExternalPAG.h"
+#include "Util/BasicTypes.h"
+#include "Util/SVFUtil.h"
+#include "Util/SVFModule.h"
+
+using namespace SVFUtil;
+
+llvm::cl::list<std::string> ExternalPAGArgs("extpags",
+                                              llvm::cl::desc("ExternalPAGs to use during PAG construction (format: func1@/path/to/graph,func2@/foo,..."),
+                                              llvm::cl::CommaSeparated);
+
+std::vector<std::pair<std::string, std::string>>
+    ExternalPAG::parseExternalPAGs(llvm::cl::list<std::string> &extpagsArgs) {
+    std::vector<std::pair<std::string, std::string>> parsedExternalPAGs;
+    for (auto arg = extpagsArgs.begin(); arg != extpagsArgs.end();
+         ++arg) {
+        std::stringstream ss(*arg);
+        std::string functionName;
+        getline(ss, functionName, '@');
+        std::string path;
+        getline(ss, path);
+        parsedExternalPAGs.push_back(
+            std::pair<std::string, std::string>(functionName, path));
+    }
+
+    return parsedExternalPAGs;
+}
+
+void ExternalPAG::initialise(SVFModule svfModule) {
+    PAG *pag = PAG::getPAG();
+
+    std::vector<std::pair<std::string, std::string>> parsedExternalPAGs
+        = ExternalPAG::parseExternalPAGs(ExternalPAGArgs);
+
+    // Build ext PAGs (and add them) first to use them in PAG construction.
+    for (auto extpagPair= parsedExternalPAGs.begin();
+         extpagPair != parsedExternalPAGs.end(); ++extpagPair) {
+        std::string fname = extpagPair->first;
+        std::string path = extpagPair->second;
+
+        ExternalPAG extpag = ExternalPAG(fname);
+        extpag.readFromFile(path);
+        extpag.addExternalPAG(svfModule.getFunction(fname));
+    }
+}
+
+bool ExternalPAG::addExternalPAG(Function *function) {
+    // The function does not exist in the module - bad arg?
+    // TODO: maybe some warning?
+    if (function == NULL) return false;
+
+    PAG *pag = PAG::getPAG();
+    if (pag->hasExternalPAG(function)) return false;
+
+    outs() << "Adding extpag " << this->getFunctionName() << "\n";
+    // Temporarily trick SVF Module into thinking we are reading from
+    // file to avoid setting BBs/Values (as these nodes don't have any).
+    std::string oldSVFModuleFileName = SVFModule::pagFileName();
+    SVFModule::setPagFromTXT("tmp");
+
+    // We need: the new nodes.
+    //        : the new edges.
+    //        : to map function names to the entry nodes.
+    //        : to map function names to the return node.
+
+    // To create the new edges.
+    std::map<NodeID, PAGNode *> extToNewNodes;
+
+    // Add the nodes.
+    for (auto extNodeIt = this->getNodes().begin();
+         extNodeIt != this->getNodes().end(); ++extNodeIt) {
+        NodeID extNodeId = std::get<0>(*extNodeIt);
+        std::string extNodeType = std::get<1>(*extNodeIt);
+
+        NodeID newNodeId;
+        if (extNodeType == "v") {
+            newNodeId = pag->addDummyValNode();
+        } else {
+            // TODO: fix obj node - there's more to it.
+            newNodeId = pag->addDummyObjNode();
+        }
+
+        extToNewNodes[extNodeId] = pag->getPAGNode(newNodeId);
+    }
+
+    // Add the edges.
+    for (auto extEdgeIt = this->getEdges().begin();
+         extEdgeIt != this->getEdges().end(); ++extEdgeIt) {
+        NodeID extSrcId = std::get<0>(*extEdgeIt);
+        NodeID extDstId = std::get<1>(*extEdgeIt);
+        std::string extEdgeType = std::get<2>(*extEdgeIt);
+        int extOffsetOrCSId = std::get<3>(*extEdgeIt);
+
+        PAGNode *srcNode = extToNewNodes[extSrcId];
+        PAGNode *dstNode = extToNewNodes[extDstId];
+        NodeID srcId = srcNode->getId();
+        NodeID dstId = dstNode->getId();
+
+        if (extEdgeType == "addr") {
+            pag->addAddrEdge(srcId, dstId);
+        } else if (extEdgeType == "copy") {
+            pag->addCopyEdge(srcId, dstId);
+        } else if (extEdgeType == "load") {
+            pag->addLoadEdge(srcId, dstId);
+        } else if (extEdgeType == "store") {
+            pag->addStoreEdge(srcId, dstId);
+        } else if (extEdgeType == "gep") {
+            pag->addNormalGepEdge(srcId, dstId, LocationSet(extOffsetOrCSId));
+        } else if (extEdgeType == "variant-gep") {
+            pag->addVariantGepEdge(srcId, dstId);
+        } else if (extEdgeType == "call") {
+            pag->addEdge(srcNode, dstNode, new CallPE(srcNode, dstNode, NULL));
+        } else if (extEdgeType == "ret") {
+            pag->addEdge(srcNode, dstNode, new RetPE(srcNode, dstNode, NULL));
+        } else {
+            outs() << "Bad edge type found during extpag addition\n";
+        }
+    }
+
+    // Record the arg nodes.
+    std::map<int, PAGNode *> argNodes;
+    for (auto argNodeIt = this->getArgNodes().begin();
+         argNodeIt != this->getArgNodes().end(); ++argNodeIt) {
+        int index = argNodeIt->first;
+        NodeID extNodeId = argNodeIt->second;
+        argNodes[index] = extToNewNodes[extNodeId];
+    }
+
+    pag->getFunctionToExternalPAGEntriesMap()[function] = argNodes;
+
+    // Record the return node.
+    if (this->hasReturnNode()) {
+        pag->getFunctionToExternalPAGReturnNodes()[function] =
+            extToNewNodes[this->getReturnNode()];
+    }
+
+    // Put it back as if nothing happened.
+    SVFModule::setPagFromTXT(oldSVFModuleFileName);
+}
+
+// Very similar implementation to the PAGBuilderFromFile.
+void ExternalPAG::readFromFile(std::string filename) {
+    std::string line;
+    std::ifstream pagFile(filename.c_str());
+
+    if (!pagFile.is_open()) {
+        outs() << "ExternalPAG::buildFromFile: could not open " << filename
+               << "\n";
+        return;
+    }
+
+    while (pagFile.good()) {
+        std::getline(pagFile, line);
+
+        Size_t tokenCount = 0;
+        std::string tmps;
+        std::istringstream ss(line);
+        while (ss.good()) {
+            ss >> tmps;
+            tokenCount++;
+        }
+
+        if (tokenCount == 0) {
+            // Empty line.
+            continue;
+        }
+
+        if (tokenCount == 2 || tokenCount == 3) {
+            // It's a node.
+            NodeID nodeId;
+            std::string nodeType;
+            std::istringstream ss(line);
+            ss >> nodeId;
+            ss >> nodeType;
+
+            if (nodeType == "v" || nodeType == "o") {
+                nodes.insert(std::tuple<NodeID, std::string>(nodeId, nodeType));
+            } else {
+                assert(false
+                       && "format not supported, please specify node type");
+            }
+
+
+            if (tokenCount == 3) {
+                // If there are 3 tokens, it should be 0, 1, 2, ... or ret.
+                std::string argNoOrRet;
+                ss >> argNoOrRet;
+
+                if (argNoOrRet == "ret") {
+                    returnNode = nodeId;
+                } else if (std::all_of(argNoOrRet.begin(), argNoOrRet.end(),
+                                       ::isdigit)) {
+                    int argNo = std::stoi(argNoOrRet);
+                    argNodes[argNo] = nodeId;
+                } else {
+                    assert(false
+                           && "format not supported, not arg number or ret");
+                }
+
+            }
+        } else if (tokenCount == 4) {
+            // It's an edge
+            NodeID nodeSrc;
+            NodeID nodeDst;
+            Size_t offsetOrCSId;
+            std::string edge;
+            std::istringstream ss(line);
+            ss >> nodeSrc;
+            ss >> edge;
+            ss >> nodeDst;
+            ss >> offsetOrCSId;
+
+            edges.insert(
+                std::tuple<NodeID, NodeID, std::string, int>(nodeSrc, nodeDst,
+                                                             edge,
+                                                             offsetOrCSId));
+        } else {
+            if (!line.empty()) {
+                outs() << "format not supported, token count = "
+                        << tokenCount << "\n";
+                assert(false && "format not supported");
+            }
+        }
+    }
+
+    /*
+    TODO: might need to include elsewhere.
+    /// new gep node's id from lower bound, nodeNum may not reflect the total nodes
+    u32_t lower_bound = 1000;
+    for(u32_t i = 0; i < lower_bound; i++)
+        pag->incNodeNum();
+    */
+}
+
