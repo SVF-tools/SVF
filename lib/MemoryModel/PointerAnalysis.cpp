ece5109eab27b4fcf699334951f2ebd3ca078010
Matthew Davis <45373823+davis-matthew@users.noreply.github.com>
2019-07-10 15:09:10 -0500

    Corrected Method Spelling
    As suggested by https://github.com/nix7965 in https://github.com/SVF-tools/SVF/issues/133

--- lib/MemoryModel/PointerAnalysis.cpp
+++ lib/MemoryModel/PointerAnalysis.cpp
@@ -247,7 +247,7 @@ void PointerAnalysis::finalize() {
     if (FuncPointerPrint)
         printIndCSTargets();
 
-    getPTACallGraph()->vefityCallGraph();
+    getPTACallGraph()->verifyCallGraph();
 
     getPTACallGraph()->dump("callgraph_final");
 
