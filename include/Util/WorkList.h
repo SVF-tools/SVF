1a479e904a9633393349df37a6baac6ac31e6d6d
Matthew Davis <45373823+davis-matthew@users.noreply.github.com>
2019-07-24 13:30:59 -0500

    Spelling fix worklist.h

--- include/Util/WorkList.h
+++ include/Util/WorkList.h
@@ -115,7 +115,7 @@ private:
 };
 
 /**
- * Worlist with "first in first out" order.
+ * Worklist with "first in first out" order.
  * New nodes will be pushed at back and popped from front.
  * Elements in the list are unique as they're recorded by std::set.
  */
