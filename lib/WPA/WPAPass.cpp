0edc74770c187229bb326007c76e926c0694ecf6
kisslune <yuxiang.lei@student.uts.edu.au>
2019-08-19 14:41:10 +1000

    scc

--- lib/WPA/WPAPass.cpp
+++ lib/WPA/WPAPass.cpp
@@ -56,7 +56,6 @@ static llvm::cl::bits<PointerAnalysis::PTATY> PASelected(llvm::cl::desc("Select 
             clEnumValN(PointerAnalysis::AndersenHCD_WPA, "hander", "Hybrid cycle detection inclusion-based analysis"),
             clEnumValN(PointerAnalysis::AndersenHLCD_WPA, "hlander", "Hybrid lazy cycle detection inclusion-based analysis"),
             clEnumValN(PointerAnalysis::AndersenSCD_WPA, "sander", "Selective cycle detection inclusion-based analysis"),
-//            clEnumValN(PointerAnalysis::AndersenDSCD_WPA, "sander", "Differential selective cycle detection inclusion-based analysis"),
             clEnumValN(PointerAnalysis::AndersenWaveDiff_WPA, "wander", "Wave propagation inclusion-based analysis"),
             clEnumValN(PointerAnalysis::AndersenWaveDiff_WPA, "ander", "Diff wave propagation inclusion-based analysis"),
             clEnumValN(PointerAnalysis::AndersenWaveDiffWithType_WPA, "andertype", "Diff wave propagation with type inclusion-based analysis"),
@@ -124,12 +123,6 @@ void WPAPass::runPointerAnalysis(SVFModule svfModule, u32_t kind)
         case PointerAnalysis::AndersenSCD_WPA:
             _pta = new AndersenSCD();
             break;
-//        case PointerAnalysis::AndersenDSCD_WPA:
-//            _pta = new AndersenDSCD();
-//            break;
-//        case PointerAnalysis::AndersenWave_WPA:
-//            _pta = new AndersenWave();
-//            break;
         case PointerAnalysis::AndersenWaveDiff_WPA:
             _pta = new AndersenWaveDiff();
             break;
