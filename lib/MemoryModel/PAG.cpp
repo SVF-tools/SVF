5bf9f4ba0371b8dec9ed383070f7f618ce8ac40f
kisslune <yuxiang.lei@student.uts.edu.au>
2020-02-14 19:23:11 +1100

    gepid

--- lib/MemoryModel/PAG.cpp
+++ lib/MemoryModel/PAG.cpp
@@ -342,9 +342,11 @@ NodeID PAG::addGepObjNode(const MemObj* obj, const LocationSet& ls) {
     assert(0==GepObjNodeMap.count(std::make_pair(base, ls))
            && "this node should not be created before");
 
-    NodeID gepMultiplier = getNodeNumAfterPAGBuild() > SymbolTableInfo::getMaxFieldLimit() ? getNodeNumAfterPAGBuild()
-                                                                                     : SymbolTableInfo::getMaxFieldLimit();
-    NodeID gepId = base * pow(10, (ceil(log10(gepMultiplier)))) + ls.getOffset();
+    NodeID gepMultiplier = pow(10, ceil(log10(
+            getNodeNumAfterPAGBuild() > SymbolTableInfo::getMaxFieldLimit() ?
+            getNodeNumAfterPAGBuild() : SymbolTableInfo::getMaxFieldLimit()
+    )));
+    NodeID gepId = ls.getOffset() * gepMultiplier + base;
     GepObjNodeMap[std::make_pair(base, ls)] = gepId;
 	GepObjPN *node = new GepObjPN(obj, gepId, ls);
     memToFieldsMap[base].set(gepId);
