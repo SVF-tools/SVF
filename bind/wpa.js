6d64683e38db8ae836591a810be3ae4d4c9ff512
Ajani Bilby <me@ajanibilby.com>
2020-07-14 18:50:33 +1000

    ~NPM rebind commands
    Using NPM to bind the commands for NPM install

---
+++ bind/wpa.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const { spawn } = require('child_process');
+const { resolve } = require('path');
+
+let cmd = spawn(resolve(__dirname, '../Release-build/bin/wpa'), process.argv.slice(2), {
+	cwd: resolve('.')
+});
+cmd.stderr.pipe(process.stderr);
+cmd.stdout.pipe(process.stdout);
+process.stdin.pipe(cmd.stdin);
