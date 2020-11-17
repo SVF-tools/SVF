75e3b7c2c34045700cbf619a64991d99c3398212
rockysui@gmail.com <rockysui@gmail.com>
2020-11-17 13:32:31 +1100

    fix getLHSTopLevPtr

--- lib/Graphs/VFG.cpp
+++ lib/Graphs/VFG.cpp
@@ -854,6 +854,12 @@ const PAGNode* VFG::getLHSTopLevPtr(const VFGNode* node) const
         return load->getPAGDstNode();
     else if(const PHIVFGNode* phi = SVFUtil::dyn_cast<PHIVFGNode>(node))
         return phi->getRes();
+    else if(const CmpVFGNode* cmp = SVFUtil::dyn_cast<CmpVFGNode>(node))
+        return cmp->getRes();
+    else if(const BinaryOPVFGNode* bop = SVFUtil::dyn_cast<BinaryOPVFGNode>(node))
+        return bop->getRes();
+    else if(const UnaryOPVFGNode* uop = SVFUtil::dyn_cast<UnaryOPVFGNode>(node))
+        return uop->getRes();
     else if(const ActualParmVFGNode* ap = SVFUtil::dyn_cast<ActualParmVFGNode>(node))
         return ap->getParam();
     else if(const FormalParmVFGNode*fp = SVFUtil::dyn_cast<FormalParmVFGNode>(node))
