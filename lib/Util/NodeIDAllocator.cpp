6dcb9da61c33107af1d12ed80cc0138cbbc15cc8
Hui Peng <peng124@purdue.edu>
2021-03-07 03:09:51 -0500

    Fix the init order in constructor of SymbolTableInfo and NodeIDA
    In the current version, the compiler emits the following warning as error:
    
    error: field 'maxStSize' will be initialized after field 'mod' [-Werror,-Wreorder-ctor]

--- lib/Util/NodeIDAllocator.cpp
+++ lib/Util/NodeIDAllocator.cpp
@@ -32,7 +32,8 @@ namespace SVF
 
     // Initialise counts to 4 because that's how many special nodes we have.
     NodeIDAllocator::NodeIDAllocator(void)
-        : numNodes(4), numObjects(4), numValues(4), numSymbols(4), strategy(Options::NodeAllocStrat)
+        : numObjects(4), numValues(4), numSymbols(4),
+          numNodes(4), strategy(Options::NodeAllocStrat)
     { }
 
     NodeID NodeIDAllocator::allocateObjectId(void)
