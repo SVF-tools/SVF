629fccde28296e8ba9329db7ca131c09724e9ab5
Richard Ford <richardlford@gmail.com>
2021-03-25 16:36:04 -0400

    Improve Interprocedure Control-Flow Graph (ICFG) debugging experience
    The changes are for the following:
    1. Probably most importantly, when the dot graph of the ICFG
       is produced, it was not showing the LLVM instruction for the
       IntraBlockNodes. This changed adds the needed code.
    2. Added a dump method for ICFGNodes which just prints the
       toString() contents to the console.
    3. Added a ICFG::view() method that will pop up a dot dump of the
       ICFG using the viewer selected by xdg-open for .dot files.
    4. It is possible one might want to view the ICFG before the
       Program Assignment Graph (PAG) has been built. However
       the ICFG stores some data related to the PAG and the
       graph writer for the ICFG tries to access the PAG.
       This change modifes PAG::getPAG by adding an additional
       parameter, skipCreate, which defaults to false.
       When the ICFG dot graph is being created, it calls
       PAG::getPAP with skipCreate false. This will return the
       PAG pointer, if it has been created, but return null if not.
       If it is null, then it skips trying to dump PAG-related
       information.

--- lib/Graphs/ICFG.cpp
+++ lib/Graphs/ICFG.cpp
@@ -64,6 +64,9 @@ const std::string ICFGNode::toString() const {
     return rawstr.str();
 }
 
+void ICFGNode::dump() const {
+    outs() << this->toString() << "\n";
+}
 
 const std::string GlobalBlockNode::toString() const {
     std::string str;
@@ -397,6 +400,14 @@ void ICFG::dump(const std::string& file, bool simple)
 }
 
 /*!
+ * View ICFG
+ */
+void ICFG::view()
+{
+    llvm::ViewGraph(this, "Interprocedural Control-Flow Graph");
+}
+
+/*!
  * Update ICFG for indirect calls
  */
 void ICFG::updateCallGraph(PTACallGraph* callgraph)
@@ -460,11 +471,15 @@ struct DOTGraphTraits<ICFG*> : public DOTGraphTraits<PAG*>
         if (IntraBlockNode* bNode = SVFUtil::dyn_cast<IntraBlockNode>(node))
         {
             rawstr << "IntraBlockNode ID: " << bNode->getId() << " \t";
-            PAG::PAGEdgeList&  edges = PAG::getPAG()->getInstPTAPAGEdgeList(bNode);
-            for (PAG::PAGEdgeList::iterator it = edges.begin(), eit = edges.end(); it != eit; ++it)
-            {
-                const PAGEdge* edge = *it;
-                rawstr << edge->toString();
+            rawstr << value2String(bNode->getInst()) << " \t";
+            PAG* pag = PAG::getPAG(false, true /* skipCreate */);
+            if (pag != nullptr) {
+                PAG::PAGEdgeList&  edges = pag->getInstPTAPAGEdgeList(bNode);
+                for (PAG::PAGEdgeList::iterator it = edges.begin(), eit = edges.end(); it != eit; ++it)
+                {
+                    const PAGEdge* edge = *it;
+                    rawstr << edge->toString();
+                }
             }
             rawstr << " {fun: " << bNode->getFun()->getName() << "}";
         }
@@ -486,11 +501,14 @@ struct DOTGraphTraits<ICFG*> : public DOTGraphTraits<PAG*>
         }
         else if (GlobalBlockNode* glob  = SVFUtil::dyn_cast<GlobalBlockNode>(node) )
         {
-            PAG::PAGEdgeList&  edges = PAG::getPAG()->getInstPTAPAGEdgeList(glob);
-            for (PAG::PAGEdgeList::iterator it = edges.begin(), eit = edges.end(); it != eit; ++it)
-            {
-                const PAGEdge* edge = *it;
-                rawstr << edge->toString();
+            PAG* pag = PAG::getPAG(false, true /* skipCreate */);
+            if (pag != nullptr) {
+                PAG::PAGEdgeList&  edges = pag->getInstPTAPAGEdgeList(glob);
+                for (PAG::PAGEdgeList::iterator it = edges.begin(), eit = edges.end(); it != eit; ++it)
+                {
+                    const PAGEdge* edge = *it;
+                    rawstr << edge->toString();
+                }
             }
         }
         else
