2f34d879e97a2131beba78526aba06207d72c59c
yuleisui <rockysui@gmail.com>
2021-05-29 10:25:43 +1000

    add assertion when creating a PAGNode (for guide DEBUG strategy in NodeIDAllocator

--- include/Graphs/PAG.h
+++ include/Graphs/PAG.h
@@ -771,13 +771,15 @@ public:
     /// Add a value (pointer) node
     inline NodeID addValNode(const Value*, PAGNode *node, NodeID i)
     {
-		assert(i<UINT_MAX && "exceeding the maximum node limits");
+        assert(hasGNode(i) == false && "This NodeID has been used and clashing here, Please check NodeIDAllocator");
+        assert(i < UINT_MAX && "exceeding the maximum node limits");
         return addNode(node,i);
     }
     /// Add a memory obj node
     inline NodeID addObjNode(const Value*, PAGNode *node, NodeID i)
     {
-		assert(i<UINT_MAX && "exceeding the maximum node limits");
+        assert(hasGNode(i) == false && "This NodeID has been used and clashing here. Please check NodeIDAllocator");
+        assert(i < UINT_MAX && "exceeding the maximum node limits");
         return addNode(node,i);
     }
     /// Add a unique return node for a procedure
