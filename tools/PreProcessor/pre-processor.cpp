df40fdc60526b6f5732c34f84566a043da5ad7b0
Nicholas Tsom <nick.tsom@gmail.com>
2021-09-29 16:39:46 +1000

    Added PreProcessor application

---
+++ tools/PreProcessor/pre-processor.cpp
@@ -0,0 +1,55 @@
+//===- svf-ex.cpp -- A driver example of SVF-------------------------------------//
+//
+//                     SVF: Static Value-Flow Analysis
+//
+// Copyright (C) <2013->  <Yulei Sui>
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===-----------------------------------------------------------------------===//
+
+/*
+ // A program to pre-process .bc files for IR Annotation
+ //
+ // Author: Nicholas Tsom
+ */
+
+#include "SVF-FE/LLVMUtil.h"
+#include "Graphs/SVFG.h"
+#include "WPA/Andersen.h"
+#include "SVF-FE/PAGBuilder.h"
+
+using namespace llvm;
+using namespace std;
+using namespace SVF;
+
+static llvm::cl::opt<std::string> InputFilename(cl::Positional,
+        llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
+
+int main(int argc, char ** argv)
+{
+
+    int arg_num = 0;
+    char **arg_value = new char*[argc];
+    std::vector<std::string> moduleNameVec;
+    SVFUtil::processArguments(argc, argv, arg_num, arg_value, moduleNameVec);
+    cl::ParseCommandLineOptions(arg_num, arg_value,
+                                "Whole Program Points-to Analysis\n");
+
+    LLVMModuleSet::getLLVMModuleSet()->preProcessBCs(moduleNameVec);
+
+    return 0;
+}
+
