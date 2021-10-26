9657e3633fef5b9be9e51eee1298ef9c951f20b0
Frank Busse <bb0xfb@gmail.com>
2021-10-26 15:48:41 +0100

    setup.sh: export SVF_DIR, use build directory instead of working directory as base

--- setup.sh
+++ setup.sh
@@ -1,32 +1,62 @@
+#!/usr/bin/env bash
+
 echo "Setting up environment for SVF"
 
 
 #########
-# export LLVM_DIR and Z3_DIR
-# Please change LLVM_DIR and Z3_DIR if they are different 
+# export SVF_DIR, LLVM_DIR and Z3_DIR
+# Please change LLVM_DIR and Z3_DIR if they are different
 ########
 
-export SVFHOME=`pwd`
-if [ -z "$LLVM_DIR" ]
-then
-   export LLVM_DIR=$SVFHOME/llvm-12.0.0.obj
+# in a local installation $SVF_DIR is the directory containing setup.sh
+SVF_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1; pwd -P)"
+export SVF_DIR
+echo "SVF_DIR=$SVF_DIR"
+
+function set_llvm {
+    # LLVM_DIR already set
+    [ -n "$LLVM_DIR" ] && return 0
+
+    # use local download directory
+    LLVM_DIR="$SVF_DIR/llvm-12.0.0.obj"
+    [ -d "$LLVM_DIR" ] && return 0
+
+    # ... otherwise don't set LLVM_DIR
+    return 1
+}
+
+if set_llvm; then
+    export LLVM_DIR
+    export PATH="$LLVM_DIR/bin:$PATH"
+    echo "LLVM_DIR=$LLVM_DIR"
+else
+    echo "- LLVM_DIR not set, probably system-wide installation"
 fi
 
-echo "LLVM_DIR =" $LLVM_DIR
 
-if [ -z "$Z3_DIR" ]
-then
-  export Z3_DIR=$SVFHOME/z3.obj
+function set_z3 {
+    # Z3_DIR already set
+    [ -n "$Z3_DIR" ] && return 0
+
+    # use local download directory
+    Z3_DIR="$SVF_DIR/z3.obj"
+    [ -d "$Z3_DIR" ] && return 0
+
+    # ... otherwise don't set Z3_DIR
+    return 1
+}
+
+if set_z3; then
+    export Z3_DIR
+    echo "Z3_DIR=$Z3_DIR"
+else
+    echo "- Z3_DIR not set, probably system-wide installation"
 fi
-
-echo "Z3_DIR =" $Z3_DIR
-
-export PATH=$LLVM_DIR/bin:$PATH
 
 
 #########
 #export PATH FOR SVF and LLVM executables
-#########                                                                 
+#########
 if [[ $1 == 'debug' ]]
 then
 PTAOBJTY='Debug'
@@ -36,7 +66,5 @@ fi
 Build=$PTAOBJTY'-build'
 
 export PATH=$LLVM_DIR/bin:$PATH
-export PTABIN=$SVFHOME/$Build/bin
+PTABIN=$SVF_DIR/$Build/bin
 export PATH=$PTABIN:$PATH
-
-
