5d74ac6fcd365667064143797e5dcc9c1c6ab1ef
Yulei Sui <rockysui@gmail.com>
2022-02-08 16:27:23 +1100

    computeGepOffset as public method

--- include/SVF-FE/SVFIRBuilder.h
+++ include/SVF-FE/SVFIRBuilder.h
@@ -205,7 +205,9 @@ public:
         // TODO: ignore here:
     }
     //}@
-
+    /// Compute offset of a gep instruction or gep constant expression
+    bool computeGepOffset(const User *V, LocationSet& ls);
+    
 protected:
     /// Handle globals including (global variable and functions)
     //@{
@@ -220,9 +222,6 @@ protected:
 
     /// Infer field index from byteoffset.
     u32_t inferFieldIdxFromByteOffset(const llvm::GEPOperator* gepOp, DataLayout *dl, LocationSet& ls, s64_t idx);
-
-    /// Compute offset of a gep instruction or gep constant expression
-    bool computeGepOffset(const User *V, LocationSet& ls);
 
     /// Get the base type and max offset
     const Type *getBaseTypeAndFlattenedFields(const Value *V, std::vector<LocationSet> &fields, const Value* sz);
