7d708680551aba3d62f1d29a2c860bc309cebb42
Talbenxu <talbenxu@gmail.com>
2022-05-23 09:23:01 +1000

    Build Error Fix

--- include/CFL/CFLGraphBuilder.h
+++ include/CFL/CFLGraphBuilder.h
@@ -42,12 +42,64 @@ class CFLGraphBuilder
 public:
     /// Build graph by copying nodes and edges from any graph inherited from GenericGraph
     template<class N, class E>
-    void build(GenericGraph<N,E>* graph, CFLGraph* cflGraph);
+    void build(GenericGraph<N,E>* graph, CFLGraph* cflGraph)
+    {
+        for(auto it = graph->begin(); it!= graph->end(); it++)
+        {
+            CFLNode* node = new CFLNode((*it).first);
+            cflGraph->addCFLNode((*it).first, node);
+        }
+        for(auto it = graph->begin(); it!= graph->end(); it++)
+        {
+            N* node = (*it).second;
+            for(E* edge : node->getOutEdges())
+            {
+                cflGraph->addCFLEdge(cflGraph->getGNode(edge->getSrcID()), cflGraph->getGNode(edge->getDstID()), edge->getEdgeKind());
+            }
+        }
+    }
 
      /// Build Bidirectional graph by copying nodes and edges from any graph inherited from GenericGraph
     template<class N, class E>
     void buildBigraph(GenericGraph<N,E>* graph, CFLGraph* cflGraph)
-
+    {
+        for(auto it = graph->begin(); it!= graph->end(); it++)
+        {
+            CFLNode* node = new CFLNode((*it).first);
+            cflGraph->addCFLNode((*it).first, node);
+        }
+        for(auto it = graph->begin(); it!= graph->end(); it++)
+        {
+            N* node = (*it).second;
+            for(E* edge : node->getOutEdges())
+            {
+                CFLGrammar::Kind edgeLabel = edge->getEdgeKind();
+                // Need to get the offset from the Const Edge
+                // The offset present edge is only from Normal Gep CG at moment
+                if(NormalGepCGEdge::classof(edge))
+                {
+                    NormalGepCGEdge *nGepEdge = SVFUtil::dyn_cast<NormalGepCGEdge>(edge);
+                    CFLGrammar::Attribute offset =  nGepEdge->getConstantFieldIdx();
+                    cflGraph->addAttribute(edgeLabel, offset);
+                    edgeLabel = CFLGrammar::getAttributedKind(offset, edgeLabel);
+                    cflGraph->addCFLEdge(cflGraph->getGNode(edge->getSrcID()), cflGraph->getGNode(edge->getDstID()), edgeLabel);
+                    std::string key = cflGraph->sym2LabelMap[edge->getEdgeKind()];
+                    key.pop_back();
+                    key.pop_back();    // _i standsfor attribute variable should place at last
+                    key.append("bar_i");   // for example Gep_i should be Gepbar_i, not Gep_ibar
+                    cflGraph->addCFLEdge(cflGraph->getGNode(edge->getDstID()), cflGraph->getGNode(edge->getSrcID()), CFLGrammar::getAttributedKind(offset, cflGraph->label2SymMap[key]));
+                    cflGraph->addAttribute(cflGraph->label2SymMap[key], offset);
+                }
+                else 
+                {
+                    cflGraph->addCFLEdge(cflGraph->getGNode(edge->getSrcID()), cflGraph->getGNode(edge->getDstID()), edgeLabel);
+                    std::string key = cflGraph->sym2LabelMap[edge->getEdgeKind()];
+                    key.append("bar");    
+                    cflGraph->addCFLEdge(cflGraph->getGNode(edge->getDstID()), cflGraph->getGNode(edge->getSrcID()), cflGraph->label2SymMap[key]);
+                }  
+            }   
+        }
+    }
     /// Build graph from file
     void build(std::string filename, CFLGraph* cflGraph);
 
