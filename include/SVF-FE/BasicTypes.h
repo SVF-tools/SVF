8052a7670d31e4cae5a9227672fe1b26dd1245c5
JasonZhongZexin <jason.zhongzexin@gmail.com>
2022-06-17 14:48:31 +0800

    refactor constantarray

--- include/SVF-FE/BasicTypes.h
+++ include/SVF-FE/BasicTypes.h
@@ -55,6 +55,7 @@ typedef llvm::ConstantDataSequential ConstantDataSequential;
 typedef llvm::ConstantExpr ConstantExpr;
 typedef llvm::ConstantDataArray ConstantDataArray;
 typedef llvm::ConstantData ConstantData;
+typedef llvm::ConstantArray ConstantArray;
 
 // LLVM Instructions
 typedef llvm::AllocaInst AllocaInst;
