7f08b804da51406b009357a61865730adda928f5
jiawei-95 <jiawei.ren95@gmail.com>
2022-07-21 16:12:28 +0800

    update

--- lib/SABER/Z3ExprManager.cpp
+++ lib/SABER/Z3ExprManager.cpp
@@ -1,6 +1,31 @@
+//===- LeakChecker.cpp -- Memory leak detector ------------------------------//
 //
-// Created by Jiawei Ren on 2022/7/15.
+//                     SVF: Static Value-Flow Analysis
 //
+// Copyright (C) <2013-2017>  <Yulei Sui>
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===----------------------------------------------------------------------===//
+
+/*
+ * Z3ExprManager.cpp
+ *
+ *  Created on: Jul 15, 2022
+ *      Author: Yulei, Xiao and Jiawei
+ */
 
 #include "Util/Z3ExprManager.h"
 #include "Util/Options.h"
