f5b9666b275611f0346b8f1ee4e6710addfee86b
Yulei Sui <rockysui@gmail.com>
2022-07-29 21:27:34 +1000

    fix #715

--- lib/MemoryModel/SymbolTableInfo.cpp
+++ lib/MemoryModel/SymbolTableInfo.cpp
@@ -594,7 +594,7 @@ bool ObjTypeInfo::isNonPtrFieldObj(const LocationSet& ls)
         else
             sz = SymbolTableInfo::SymbolInfo()->getStructInfoIter(ety)->second->getFlattenFieldTypes().size();
 
-        if(sz <= ls.accumulateConstantFieldIdx()){
+        if(sz <= (u32_t)ls.accumulateConstantFieldIdx()){
             writeWrnMsg("out of bound error when accessing the struct/array");
             return false;
         }
