0e9c4b22be7ef57aff126bae5212581271e04477
shuangxiang kan <18550887212@163.com>
2022-07-31 14:53:03 +1000

    fix "https://github.com/SVF-tools/SVF/issues/802" and add license to ExtAPI.h/cpp

--- lib/Util/ExtAPI.cpp
+++ lib/Util/ExtAPI.cpp
@@ -1,11 +1,31 @@
-/*  [ExtAPI.cpp] The actual database of external functions
- *  v. 005, 2008-08-08
- *------------------------------------------------------------------------------
- */
+//===- ExtAPI.cpp -- External functions -----------------------------------------//
+//
+//                     SVF: Static Value-Flow Analysis
+//
+// Copyright (C) <2013-2017>  <Yulei Sui>
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===----------------------------------------------------------------------===//
 
 /*
- * Modified by Yulei Sui 2013
-*/
+ * ExtAPI.cpp
+ *
+ *  Created on: July 1, 2022
+ *      Author: Shuangxiang Kan
+ */
 
 #include "Util/ExtAPI.h"
 #include "Util/SVFUtil.h"
