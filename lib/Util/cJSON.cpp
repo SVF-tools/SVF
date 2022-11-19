8b8104b9f4636e99f0b9d783ce6c5d6f178f408a
shuangxiang kan <18550887212@163.com>
2022-11-19 18:48:17 +1100

    Fix snprintf()

--- lib/Util/cJSON.cpp
+++ lib/Util/cJSON.cpp
@@ -562,22 +562,22 @@ static cJSON_bool print_number(const cJSON * const item, printbuffer * const out
     /* This checks for NaN and Infinity */
     if (isnan(d) || isinf(d))
     {
-        length = snprintf((char*)number_buffer, sizeof((char*)number_buffer), "null");
+        length = snprintf((char*)number_buffer, sizeof(number_buffer), "null");
     }
     else if(d == (double)item->valueint)
     {
-        length = snprintf((char*)number_buffer, sizeof((char*)number_buffer), "%d", item->valueint);
+        length = snprintf((char*)number_buffer, sizeof(number_buffer), "%d", item->valueint);
     }
     else
     {
         /* Try 15 decimal places of precision to avoid nonsignificant nonzero digits */
-        length = snprintf((char*)number_buffer, sizeof((char*)number_buffer), "%1.15g", d);
+        length = snprintf((char*)number_buffer, sizeof(number_buffer), "%1.15g", d);
 
         /* Check whether the original double can be recovered */
         if ((sscanf((char*)number_buffer, "%lg", &test) != 1) || !compare_double((double)test, d))
         {
             /* If not, print with 17 decimal places of precision */
-            length = snprintf((char*)number_buffer, sizeof((char*)number_buffer), "%1.17g", d);
+            length = snprintf((char*)number_buffer, sizeof(number_buffer), "%1.17g", d);
         }
     }
 
@@ -1010,7 +1010,7 @@ static cJSON_bool print_string_ptr(const unsigned char * const input, printbuffe
                 break;
             default:
                 /* escape and print as unicode codepoint */
-                snprintf((char*)output_pointer, sizeof((char*)output_pointer), "u%04x", *input_pointer);
+                snprintf((char*)output_pointer, sizeof(output_pointer), "u%04x", *input_pointer);
                 output_pointer += 4;
                 break;
             }
