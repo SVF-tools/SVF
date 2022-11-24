c5d40e452ec6e6247fd2bc822afc1b0ff046f4ab
jiawei.wang <jiawei.wang@horizon.ai>
2022-11-24 15:08:01 +0800

    1) add AbstractExecution header folder 2) add AbstractExecution lib folder 3) add WTO and SVFIR translator 4) TODO: some compile error in overload funcs in IntervalExeState.h

---
+++ lib/AbstractExecution/ExeState.cpp
@@ -0,0 +1,90 @@
+//===- ExeState.cpp ----General Execution States-------------------------//
+//
+//                     SVF: Static Value-Flow Analysis
+//
+// Copyright (C) <2013-2022>  <Yulei Sui>
+//
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//===----------------------------------------------------------------------===//
+/*
+ * ExeState.cpp
+ *
+ *  Created on: Aug 4, 2022
+ *      Author: Xiao Cheng
+ *
+ */
+
+#include "AbstractExecution/ExeState.h"
+#include "AbstractExecution/IntervalExeState.h"
+
+using namespace SVF;
+
+bool ExeState::operator==(const ExeState &rhs) const {
+    return eqVarToVAddrs(_varToVAddrs, rhs._varToVAddrs) && eqVarToVAddrs(_locToVAddrs, rhs._locToVAddrs);
+}
+
+void ExeState::joinWith(const ExeState &other) {
+    for (auto it = other._varToVAddrs.begin(); it != other._varToVAddrs.end(); ++it) {
+        auto key = it->first;
+        auto oit = _varToVAddrs.find(key);
+        if (oit != _varToVAddrs.end()) {
+            oit->second.join_with(it->second);
+        } else {
+            _varToVAddrs.emplace(key, it->second);
+        }
+    }
+    for (auto it = other._locToVAddrs.begin(); it != other._locToVAddrs.end(); ++it) {
+        auto key = it->first;
+        auto oit = _locToVAddrs.find(key);
+        if (oit != _locToVAddrs.end()) {
+            oit->second.join_with(it->second);
+        } else {
+            _locToVAddrs.emplace(key, it->second);
+        }
+    }
+}
+
+
+void ExeState::meetWith(const ExeState &other) {
+    for (auto it = other._varToVAddrs.begin(); it != other._varToVAddrs.end(); ++it) {
+        auto key = it->first;
+        auto oit = _varToVAddrs.find(key);
+        if (oit != _varToVAddrs.end()) {
+            oit->second.meet_with(it->second);
+        }
+    }
+    for (auto it = other._locToVAddrs.begin(); it != other._locToVAddrs.end(); ++it) {
+        auto key = it->first;
+        auto oit = _locToVAddrs.find(key);
+        if (oit != _locToVAddrs.end()) {
+            oit->second.meet_with(it->second);
+        }
+    }
+}
+
+u32_t ExeState::hash() const {
+    size_t h = getVarToVAddrs().size() * 2;
+    Hash<u32_t> hf;
+    for (const auto &t: getVarToVAddrs()) {
+        h ^= hf(t.first) + 0x9e3779b9 + (h << 6) + (h >> 2);
+    }
+    size_t h2 = getLocToVAddrs().size() * 2;
+    for (const auto &t: getLocToVAddrs()) {
+        h2 ^= hf(t.first) + 0x9e3779b9 + (h2 << 6) + (h2 >> 2);
+    }
+    Hash<std::pair<u32_t, u32_t>> pairH;
+    return pairH(std::make_pair(h, h2));
+}
