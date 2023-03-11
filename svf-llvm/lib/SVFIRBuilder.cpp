4d1ed343480f0d3d3df6754663eb4da1e70c6505
jumormt <jumormt@163.com>
2023-03-11 22:20:13 +1100

    only update for constant single type

--- svf-llvm/lib/SVFIRBuilder.cpp
+++ svf-llvm/lib/SVFIRBuilder.cpp
@@ -312,13 +312,13 @@ bool SVFIRBuilder::computeGepOffset(const User *V, LocationSet& ls)
             // If its point-to target is single value (pointer arithmetic), then it's a variant gep (%result = gep i8* %p, i32 %non-const-index)
             if(!op && gepTy->isPointerTy() && getPtrElementType(SVFUtil::dyn_cast<PointerType>(gepTy))->isSingleValueType())
                 isConst = false;
-            else {
+            if(op) {
                 // The actual index
                 s32_t idx = op->getSExtValue();
 
                 // For pointer arithmetic we ignore the byte offset
                 // consider using inferFieldIdxFromByteOffset(geopOp,dataLayout,ls,idx)?
-                 ls.setFldIdx(ls.getConstantFieldIdx() + idx);
+                ls.setFldIdx(ls.getConstantFieldIdx() + idx);
             }
         }
     }
