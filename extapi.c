0cdc02b145dcf37736f3f72b25be5b09eaab0e84
jiawei.wang <jiawei.wang@horizon.ai>
2023-06-21 19:35:52 +1000

    add extapi bc file

---
+++ extapi.c
@@ -0,0 +1,50 @@
+extern void check_overflow(char*, int sz);
+
+#define SVF_MEMCPY(dst, src, sz) \
+    check_overflow(dst, sz); \
+    check_overflow(src, sz); \
+    int i; \
+    for (i = 0; i < sz; i++) { \
+        dst[i] = src[i]; \
+    }
+
+#define SVF_MEMSET(dst, elem, sz) \
+    check_overflow(dst, sz); \
+    int i; \
+    for (i = 0; i < sz; i++) { \
+        dst[i] = elem; \
+    }
+
+
+//llvm.memcpy.p0i8.p0i8.i64
+void svf_llvm_memcpy_p0i8_p0i8_i64(char* dst, char* src, int sz) {
+    SVF_MEMCPY(dst, src, sz);
+}
+
+void svf_llvm_memmove_p0i8_p0i8_i64(char* dst, char* src, int sz) {
+    SVF_MEMCPY(dst, src, sz);
+}
+
+void svf___memcpy_chk(char* dst, char* src, int sz) {
+    SVF_MEMCPY(dst, src, sz);
+}
+
+void svf___memmove_chk(char* dst, char* src, int sz) {
+    SVF_MEMCPY(dst, src, sz);
+}
+
+void svf_llvm_memset(char* dst, char elem, int sz) {
+    SVF_MEMSET(dst, elem, sz);
+}
+
+void svf_llvm_memset_p0i8_i32(char* dst, char elem, int sz) {
+    SVF_MEMSET(dst, elem, sz);
+}
+
+void svf_llvm_memset_p0i8_i64(char* dst, char elem, int sz) {
+    SVF_MEMSET(dst, elem, sz);
+}
+
+void svf___memset_chk(char* dst, char elem, int sz) {
+    SVF_MEMSET(dst, elem, sz);
+}
