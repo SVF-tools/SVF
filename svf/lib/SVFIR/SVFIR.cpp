a9f22e7cdf9f7ac87624bbad741dfba67a230202
JasonZhongZexin <jason@PC-20220224QJOH.localdomain>
2023-08-21 13:23:05 +0800

    fix err

--- svf/lib/SVFIR/SVFIR.cpp
+++ svf/lib/SVFIR/SVFIR.cpp
@@ -470,10 +470,15 @@ void SVFIR::readGepObjNodeFromFile(const NodeID& base, const APOffset& apOffset,
         obj = gepNode->getMemObj();
     else if (FIObjVar* baseNode = SVFUtil::dyn_cast<FIObjVar>(node))
         obj = baseNode->getMemObj();
-    if (DummyObjVar* baseNode = SVFUtil::dyn_cast<DummyObjVar>(node))
+    else if (DummyObjVar* baseNode = SVFUtil::dyn_cast<DummyObjVar>(node))
         obj = baseNode->getMemObj();
-    GepObjVar *gepNode = new GepObjVar(obj, gepId, apOffset);
-    addObjNode(obj->getValue(), gepNode, gepId);
+    else
+        assert(false && "new gep obj node kind?"); 
+    if (obj != nullptr)
+    {
+        GepObjVar *gepNode = new GepObjVar(obj, gepId, apOffset);
+        addObjNode(obj->getValue(), gepNode, gepId);
+    }
 }
 
 /*!
