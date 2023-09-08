54967658ef820f062dbbd85ddaaf2a9b4a1bd01e
shuangxiang kan <18550887212@163.com>
2023-09-08 16:33:37 +1000

    Return "STATIC_OBJECT" in an external function which has a static var that its return value points to

--- svf-llvm/lib/extapi.c
+++ svf-llvm/lib/extapi.c
@@ -1,5 +1,5 @@
-// #include <stdio.h>
 #define NULL ((void *)0)
+#define STATIC_OBJECT malloc(10)
 
 /*
     Functions with __attribute__((annotate("XXX"))) will be handle by SVF specifcially.
@@ -7,7 +7,6 @@
 
     The description of methodProperties is as follows:
 
-        SVF,          // inform SVF that this function is managed by SVF and should not use its body.
         ALLOC_RET,    // returns a ptr to a newly allocated object
         ALLOC_ARGi    // stores a pointer to an allocated object in *argi
         REALLOC_RET,  
@@ -15,216 +14,448 @@
         MEMCPY,       // memset() operations
         OVERWRITE,    // svf function overwrite app function
 */
-__attribute__((annotate("ALLOC_RET")))
-__attribute__((annotate("OVERWRITE")))
-void *SyGetmem(unsigned long size)
+
+void * __rawmemchr(const void * s, int c)
 {
-    return NULL;
+    return (void *)s;
+}
+
+struct jpeg_error_mgr *jpeg_std_error(struct jpeg_error_mgr * a)
+{
+    return a;
+}
+
+char *fgets(char *str, int n, void *stream)
+{
+    return str;
+}
+
+void *memchr(const void *str, int c, unsigned long n)
+{
+    return (void *)str;
+}
+
+void * mremap(void * old_address, unsigned long old_size, unsigned long new_size, int flags)
+{
+    return old_address;
+}
+
+char *strchr(const char *str, int c)
+{
+    return (char *)str;
+}
+
+char *strerror_r(int errnum, char *buf, unsigned long buflen)
+{
+    return buf;
+}
+
+char *strpbrk(const char *str1, const char *str2)
+{
+    return (char *)str1;
+}
+
+char *strptime(const void* s, const void* format, void* tm)
+{
+    return (char *)s;
+}
+
+char *strrchr(const char *str, int c)
+{
+    return (char *)str;
+}
+
+char *strstr(const char *haystack, const char *needle)
+{
+    return (char *)haystack;
+}
+
+char *tmpnam_r(char *s)
+{
+    return s;
+}
+
+int isalnum(int character)
+{
+    return character;
+}
+
+int isalpha(int character)
+{
+    return character;
+}
+
+int isblank(int character)
+{
+    return character;
+}
+
+int iscntrl(int c)
+{
+    return c;
+}
+
+int isdigit(int c)
+{
+    return c;
+}
+
+int isgraph(int c)
+{
+    return c;
+}
+
+int islower( int arg )
+{
+    return arg;
+}
+
+int isprint(int c)
+{
+    return c;
+}
+
+int ispunct(int argument)
+{
+    return argument;
+}
+
+int isspace(char c)
+{
+    return c;
+}
+
+int isupper(int c)
+{
+    return c;
+}
+
+int isxdigit(int c)
+{
+    return c;
+}
+
+char *asctime_r(const void *tm, char *buf)
+{
+    return buf;
+}
+
+void *bsearch(const void *key, const void *base, unsigned long nitems, unsigned long size, int (*compar)(const void *, const void *))
+{
+    return (void *)base;
+}
+
+struct mntent *getmntent_r(void *fp, struct mntent *mntbuf, char *buf, int buflen)
+{
+    return mntbuf;
+}
+
+struct tm *gmtime_r(const void *timer, struct tm *buf)
+{
+    return buf;
+}
+
+char * gzgets(void* file, char * buf, int len)
+{
+    return buf;
+}
+
+struct tm *localtime_r(const void *timep, struct tm *result)
+{
+    return result;
+}
+
+char *realpath(const char *restrict path, char *restrict resolved_path)
+{
+    return resolved_path;
+}
+
+void* freopen64( const char* voidname, const char* mode, void* fp )
+{
+    return fp;
+}
+
+void* freopen(const char* voidname, const char* mode, void* fp)
+{
+    return fp;
+}
+
+const char *inet_ntop(int af, const void *restrict src,  char *restrict dst, unsigned int size)
+{
+    return dst;
+}
+
+double strtod(const char *str, char **endptr)
+{
+    *endptr = (char *)str;
+    return 0.0;
+}
+
+float strtof(const char *nptr, char **endptr)
+{
+    *endptr = (char *)nptr;
+    return 0.0;
+}
+
+long int strtol(const char *str, char **endptr, int base)
+{
+    *endptr = (char *)str;
+    return 0;
+}
+
+long double strtold(const char* str, char** endptr)
+{
+    *endptr = (char *)str;
+    return 0.0;
+}
+
+unsigned long int strtoul(const char *str, char **endptr, int base)
+{
+    *endptr = (char *)str;
+    return 0;
+}
+
+int readdir_r(void *__restrict__dir, void *__restrict__entry, void **__restrict__result)
+{
+    __restrict__entry = *__restrict__result;   
+    return 0;
+}
+
+int getpwnam_r(const char *name, void *pwd, char *buf, unsigned long buflen, void **result)
+{
+    *result = pwd;
+    return 0;
+}
+
+int getpwuid_r(unsigned int uid, void *pwd, char *buf, unsigned long buflen, void **result)
+{
+    *result = pwd;
+    return 0;
+}
+
+void _ZNSt8__detail15_List_node_base7_M_hookEPS0_(void *arg0, void **arg1)
+{
+    *arg1 = arg0;
+}
+
+void* __dynamic_cast(void* source, const void* sourceTypeInfo, const void* targetTypeInfo, unsigned long castType)
+{
+    return source;
+}
+
+void _ZNSsC1EPKcRKSaIcE(void **arg0, void *arg1)
+{
+    *arg0 = arg1;
+}
+
+void _ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1EPKcRKS3_(void **arg0, void *arg1)
+{
+    *arg0 = arg1;
 }
 
 const unsigned short **__ctype_b_loc(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 int **__ctype_tolower_loc(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 int **__ctype_toupper_loc(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 int *__errno_location(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 int * __h_errno_location(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 void* __res_state(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char *asctime(const void *timeptr)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * bindtextdomain(const char * domainname, const char * dirname)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * bind_textdomain_codeset(const char * domainname, const char * codeset)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char *ctermid(char *s)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * dcgettext(const char * domainname, const char * msgid, int category)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * dgettext(const char * domainname, const char * msgid)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * dngettext(const char * domainname, const char * msgid, const char * msgid_plural, unsigned long int n)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 void *fdopen(int fd, const char *mode)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct group *getgrgid(unsigned int gid)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct group *getgrnam(const char *name)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct hostent *gethostbyaddr(const void *addr, unsigned int len, int type)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct hostent *gethostbyname(const char *name)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct hostent *gethostbyname2(const char *name, int af)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct mntent *getmntent(void *stream)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct protoent *getprotobyname(const char *name)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct protoent *getprotobynumber(int proto)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct passwd *getpwent(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct passwd *getpwnam(const char *name)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct passwd *getpwuid(unsigned int uid)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct servent *getservbyname(const char *name, const char *proto)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct servent *getservbyport(int port, const char *proto)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct spwd *getspnam(const char *name)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * gettext(const char * msgid)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct tm *gmtime(const void *timer)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 const char *gnu_get_libc_version(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 const char * gnutls_check_version(const char * req_version)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct lconv *localeconv(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 struct tm *localtime(const void *timer)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char * ngettext(const char * msgid, const char * msgid_plural, unsigned long int n)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 void *pango_cairo_font_map_get_default(void)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char *re_comp(const char *regex)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char *setlocale(int category, const char *locale)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char *tgoto(const char *cap, int col, int row)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 char *tparm(char *str, ...)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 const char *zError(int a)
 {
-    return malloc(10);
+    return STATIC_OBJECT;
 }
 
 __attribute__((annotate("ALLOC_RET")))
@@ -871,242 +1102,9 @@ void* _ZNSt5arrayIPK1ALm2EE4backEv(void *arg)
     return ptr2;
 }
 
-
-void * __rawmemchr(const void * s, int c)
+__attribute__((annotate("ALLOC_RET")))
+__attribute__((annotate("OVERWRITE")))
+void *SyGetmem(unsigned long size)
 {
-    return (void *)s;
-}
-
-struct jpeg_error_mgr *jpeg_std_error(struct jpeg_error_mgr * a)
-{
-    return a;
-}
-
-char *fgets(char *str, int n, void *stream)
-{
-    return str;
-}
-
-void *memchr(const void *str, int c, unsigned long n)
-{
-    return (void *)str;
-}
-
-void * mremap(void * old_address, unsigned long old_size, unsigned long new_size, int flags)
-{
-    return old_address;
-}
-
-char *strchr(const char *str, int c)
-{
-    return (char *)str;
-}
-
-char *strerror_r(int errnum, char *buf, unsigned long buflen)
-{
-    return buf;
-}
-
-char *strpbrk(const char *str1, const char *str2)
-{
-    return (char *)str1;
-}
-
-char *strptime(const void* s, const void* format, void* tm)
-{
-    return (char *)s;
-}
-
-char *strrchr(const char *str, int c)
-{
-    return (char *)str;
-}
-
-char *strstr(const char *haystack, const char *needle)
-{
-    return (char *)haystack;
-}
-
-char *tmpnam_r(char *s)
-{
-    return s;
-}
-
-int isalnum(int character)
-{
-    return character;
-}
-
-int isalpha(int character)
-{
-    return character;
-}
-
-int isblank(int character)
-{
-    return character;
-}
-
-int iscntrl(int c)
-{
-    return c;
-}
-
-int isdigit(int c)
-{
-    return c;
-}
-
-int isgraph(int c)
-{
-    return c;
-}
-
-int islower( int arg )
-{
-    return arg;
-}
-
-int isprint(int c)
-{
-    return c;
-}
-
-int ispunct(int argument)
-{
-    return argument;
-}
-
-int isspace(char c)
-{
-    return c;
-}
-
-int isupper(int c)
-{
-    return c;
-}
-
-int isxdigit(int c)
-{
-    return c;
-}
-
-char *asctime_r(const void *tm, char *buf)
-{
-    return buf;
-}
-
-void *bsearch(const void *key, const void *base, unsigned long nitems, unsigned long size, int (*compar)(const void *, const void *))
-{
-    return (void *)base;
-}
-
-struct mntent *getmntent_r(void *fp, struct mntent *mntbuf, char *buf, int buflen)
-{
-    return mntbuf;
-}
-
-struct tm *gmtime_r(const void *timer, struct tm *buf)
-{
-    return buf;
-}
-
-char * gzgets(void* file, char * buf, int len)
-{
-    return buf;
-}
-
-struct tm *localtime_r(const void *timep, struct tm *result)
-{
-    return result;
-}
-
-char *realpath(const char *restrict path, char *restrict resolved_path)
-{
-    return resolved_path;
-}
-
-void* freopen64( const char* voidname, const char* mode, void* fp )
-{
-    return fp;
-}
-
-void* freopen(const char* voidname, const char* mode, void* fp)
-{
-    return fp;
-}
-
-const char *inet_ntop(int af, const void *restrict src,  char *restrict dst, unsigned int size)
-{
-    return dst;
-}
-
-double strtod(const char *str, char **endptr)
-{
-    *endptr = (char *)str;
-    return 0.0;
-}
-
-float strtof(const char *nptr, char **endptr)
-{
-    *endptr = (char *)nptr;
-    return 0.0;
-}
-
-long int strtol(const char *str, char **endptr, int base)
-{
-    *endptr = (char *)str;
-    return 0;
-}
-
-long double strtold(const char* str, char** endptr)
-{
-    *endptr = (char *)str;
-    return 0.0;
-}
-
-unsigned long int strtoul(const char *str, char **endptr, int base)
-{
-    *endptr = (char *)str;
-    return 0;
-}
-
-int readdir_r(void *__restrict__dir, void *__restrict__entry, void **__restrict__result)
-{
-    __restrict__entry = *__restrict__result;   
-    return 0;
-}
-
-int getpwnam_r(const char *name, void *pwd, char *buf, unsigned long buflen, void **result)
-{
-    *result = pwd;
-    return 0;
-}
-
-int getpwuid_r(unsigned int uid, void *pwd, char *buf, unsigned long buflen, void **result)
-{
-    *result = pwd;
-    return 0;
-}
-
-
-void _ZNSt8__detail15_List_node_base7_M_hookEPS0_(void *arg0, void **arg1)
-{
-    *arg1 = arg0;
-}
-
-void* __dynamic_cast(void* source, const void* sourceTypeInfo, const void* targetTypeInfo, unsigned long castType)
-{
-    return source;
-}
-
-void _ZNSsC1EPKcRKSaIcE(void **arg0, void *arg1)
-{
-    *arg0 = arg1;
-}
-
-void _ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEC1EPKcRKS3_(void **arg0, void *arg1)
-{
-    *arg0 = arg1;
+    return NULL;
 }
