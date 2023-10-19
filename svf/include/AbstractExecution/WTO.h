d00d4052633e059d3c2560ec1661ff1a18851173
jiawei.wang <jiawei.wang@horizon.ai>
2023-10-19 12:32:46 +1100

    temp fix wto

--- svf/include/AbstractExecution/WTO.h
+++ svf/include/AbstractExecution/WTO.h
@@ -797,7 +797,6 @@ protected:
         loop = false;
         if (const CallICFGNode* callNode = SVFUtil::dyn_cast<CallICFGNode>(node->getICFGNodes().front())) {
             const CFBasicBlockNode *succ = _graph->getCFBasicBlockNode(callNode->getRetICFGNode()->getId());
-            std::cout << "WTO visit ID:" << succ->getId() << ", func: " << succ->getFunction()->getName()  << std::endl;
             CycleDepthNumber succ_dfn = getCDN(succ);
             if (succ_dfn == CycleDepthNumber(0))
             {
@@ -818,7 +817,6 @@ protected:
                 const CFBasicBlockNode *succ = (*it)->getDstNode();
                 if (succ->getFunction() != node->getFunction())
                     continue;
-                std::cout << "WTO visit ID:" << succ->getId() << ", func: " << succ->getFunction()->getName()  << std::endl;
                 CycleDepthNumber succ_dfn = getCDN(succ);
                 if (succ_dfn == CycleDepthNumber(0))
                 {
