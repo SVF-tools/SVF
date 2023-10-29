8f08c3eda29a7380d360833d07a54aef4eecd4b0
zhongzexin.zzx <zhongzexin.zzx@alibaba-inc.com>
2023-10-30 00:23:17 +0800

    fix mac dynlib build failed

--- build.sh
+++ build.sh
@@ -225,7 +225,7 @@ mkdir "${BUILD_DIR}"
 cmake -D CMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}" \
     -DSVF_ENABLE_ASSERTIONS:BOOL=true            \
     -DSVF_SANITIZE="${SVF_SANITIZER}"            \
-    -DBUILD_SHARED_LIBS=off                      \
+    -DBUILD_SHARED_LIBS=on                      \
     -S "${SVFHOME}" -B "${BUILD_DIR}"
 cmake --build "${BUILD_DIR}" -j ${jobs}
 
