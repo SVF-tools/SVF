160f1e57c6f5e4fa39a2bfac2c89019121e5053d
jumormt <jumormt@163.com>
2023-11-15 16:34:49 +1100

    Bump LLVM from 14 to 15:  1. use llvm 15 in build.sh  2. Fix deprecated API getpointerelementtype  3. build extapi.bc with opaque ptr disabled for now

--- svf-llvm/include/SVF-LLVM/LLVMUtil.h
+++ svf-llvm/include/SVF-LLVM/LLVMUtil.h
@@ -384,11 +384,11 @@ void removeUnusedFuncsAndAnnotationsAndGlobalVariables(std::vector<Function*> re
 inline u32_t SVFType2ByteSize(const SVFType* type)
 {
     const llvm::Type* llvm_rhs = LLVMModuleSet::getLLVMModuleSet()->getLLVMType(type);
-    u32_t llvm_rhs_size = LLVMUtil::getTypeSizeInBytes(llvm_rhs->getPointerElementType());
+    u32_t llvm_rhs_size = LLVMUtil::getTypeSizeInBytes(llvm_rhs->getNonOpaquePointerElementType());
     u32_t llvm_elem_size = -1;
-    if (llvm_rhs->getPointerElementType()->isArrayTy() && llvm_rhs_size > 0)
+    if (llvm_rhs->getNonOpaquePointerElementType()->isArrayTy() && llvm_rhs_size > 0)
     {
-        size_t array_len = llvm_rhs->getPointerElementType()->getArrayNumElements();
+        size_t array_len = llvm_rhs->getNonOpaquePointerElementType()->getArrayNumElements();
         llvm_elem_size = llvm_rhs_size / array_len;
     }
     else
