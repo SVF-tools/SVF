47b41817be1552119bdff877af5885429be4afad
jiawei.wang <jiawei.wang@horizon.ai>
2023-11-17 23:49:39 +1100

    fix if offsetVarAndGepTypePair.second is nullptr since it is from ExternalAPI

--- svf/lib/MemoryModel/AccessPath.cpp
+++ svf/lib/MemoryModel/AccessPath.cpp
@@ -112,6 +112,9 @@ APOffset AccessPath::computeConstantByteOffset() const
         ///     i = 0, type: [10 x i8]*, PtrType, op = 0
         ///     i = 1, type: [10 x i8], ArrType, op = 8
         const SVFType* type = offsetVarAndGepTypePairs[i].second;
+        /// if offsetVarAndGepTypePairs[i].second is nullptr, it means
+        /// this GepStmt comes from external API, assert
+        assert(type && "this GepStmt comes from ExternalAPI cannot call this api");
         const SVFType* type2 = type;
         if (const SVFArrayType* arrType = SVFUtil::dyn_cast<SVFArrayType>(type))
         {
