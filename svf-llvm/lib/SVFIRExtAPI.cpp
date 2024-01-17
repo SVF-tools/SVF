afbb2a626ac7ee74d9ea7916068a373478f8bcd6
Jiawei Wang <jackwang834@gmail.com>
2024-01-17 16:37:31 +1100

    add arraySize to AddrStmt (#1324)
    * add arraySize to AddrStmt
    
    1) add addAddrWithAllocArraySz, the arg is AllocaInst and CallBase
    2) add arraySize getter/setter to AddrStmt
    
    * fix argsize of ext call
    
    * rename
    
    * rename
    
    * remove setCurLoc
    
    ---------
    
    Co-authored-by: bjjwwang <bjjwwang@github.com>

--- svf-llvm/lib/SVFIRExtAPI.cpp
+++ svf-llvm/lib/SVFIRExtAPI.cpp
@@ -133,7 +133,7 @@ void SVFIRBuilder::handleExtCall(const CallBase* cs, const SVFFunction* svfCalle
     {
         NodeID val = pag->getValueNode(svfInst);
         NodeID obj = pag->getObjectNode(svfInst);
-        addAddrEdge(obj, val);
+        addAddrWithHeapSz(obj, val, cs);
     }
     else if (isHeapAllocExtCallViaArg(svfCall))
     {
@@ -146,7 +146,7 @@ void SVFIRBuilder::handleExtCall(const CallBase* cs, const SVFFunction* svfCalle
             NodeID obj = pag->addDummyObjNode(arg->getType());
             if (vnArg && dummy && obj)
             {
-                addAddrEdge(obj, dummy);
+                addAddrWithHeapSz(obj, dummy, cs);
                 addStoreEdge(dummy, vnArg);
             }
         }
