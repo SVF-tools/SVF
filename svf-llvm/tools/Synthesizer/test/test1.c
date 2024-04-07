4a242701949a23def9f1a44627f91dc50a73e5e3
hanquanjushi <2982712752@qq.com>
2024-04-07 07:15:03 +0000

    add

--- svf-llvm/tools/Synthesizer/test/test1.c
+++ svf-llvm/tools/Synthesizer/test/test1.c
@@ -1,6 +1,15 @@
+int fib (int n){
+    if (n<0){
+        return -1;
+    }
+    if (n<=1){
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
 int main(){
     int a =1;
     int b =2;
-    int c = a+b;
+    int c = fib (a+b);
     return c;
 }
