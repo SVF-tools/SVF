6063ffe0e433f3977d9834972d14532e56a498af
hanquanjushi <2982712752@qq.com>
2024-04-07 03:29:26 +0000

    add

--- Download/cmake-3.29.1-linux-x86_64/share/cmake-3.29/Modules/Platform/Linux-Apple-Swift.cmake
+++
@@ -1,5 +0,0 @@
-# Linker Selection
-# BFD is known to mislink Swift objects resulting in missing type info
-set(CMAKE_Swift_USING_LINKER_SYSTEM "")
-set(CMAKE_Swift_USING_LINKER_GOLD "-use-ld=gold")
-set(CMAKE_Swift_USING_LINKER_LLD "-use-ld=lld")
