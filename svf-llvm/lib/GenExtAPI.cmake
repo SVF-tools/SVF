28c1eda8a0ab6539e35e4537245755de5200ddc0
Johannes Blaser <j.blaser@vu.nl>
2024-05-09 16:14:33 +0000

    Moved cmake files into separate directory; defined installable package for SVF; correctly defined transitive dependencies on Z3 and LLVM in the exported config

--- svf-llvm/lib/GenExtAPI.cmake
+++ svf-llvm/lib/GenExtAPI.cmake
@@ -1,7 +1,4 @@
 # Set base variables and input/output structures
-set(SVF_EXTAPI_SRC_NAME extapi.c)
-set(SVF_EXTAPI_IN_FILE ${CMAKE_CURRENT_LIST_DIR}/${SVF_EXTAPI_SRC_NAME})
-set(SVF_EXTAPI_OUT_FILE ${CMAKE_BINARY_DIR}/lib/${SVF_EXTAPI_OUT_NAME})
 set(SVF_EXTAPI_FLAGS
     -w
     -S
@@ -13,11 +10,14 @@ set(SVF_EXTAPI_FLAGS
     -Xclang
     -disable-O0-optnone)
 
+set(EXTAPI_SRC_FILE "${CMAKE_CURRENT_LIST_DIR}/extapi.c")
+set(EXTAPI_BIN_FILE "${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/extapi.bc")
+
 if(NOT ${SVF_ENABLE_OPAQUE_POINTERS})
     list(APPEND SVF_EXTAPI_FLAGS -Xclang -no-opaque-pointers)
 endif()
 
-if(NOT EXISTS ${SVF_EXTAPI_IN_FILE})
+if(NOT EXISTS "${EXTAPI_SRC_FILE}")
     message(FATAL_ERROR "Failed to find ExtAPI source file!")
 endif()
 
@@ -25,17 +25,17 @@ endif()
 find_program(
     LLVM_CLANG
     NAMES clang
-    HINTS ${LLVM_BINARY_DIR}
-    PATH_SUFFIXES bin REQUIRED)
+    HINTS "${LLVM_BINARY_DIR}"
+    PATH_SUFFIXES "bin" REQUIRED)
 
 # Define a custom command to compile extapi.c to LLVM IR bitcode in extapi.bc (in the build directory)
 add_custom_command(
-    OUTPUT ${SVF_EXTAPI_OUT_FILE}
-    COMMAND ${LLVM_CLANG} ${SVF_EXTAPI_FLAGS} -o ${SVF_EXTAPI_OUT_FILE} ${SVF_EXTAPI_IN_FILE}
-    DEPENDS ${SVF_EXTAPI_IN_FILE})
+    OUTPUT "${EXTAPI_BIN_FILE}"
+    COMMAND ${LLVM_CLANG} ${SVF_EXTAPI_FLAGS} -o ${EXTAPI_BIN_FILE} ${EXTAPI_SRC_FILE}
+    DEPENDS ${EXTAPI_SRC_FILE})
 
 # Add a custom target for generating the LLVM bytecode file (and add it to the default build targets)
-add_custom_target(gen_extapi_ir ALL DEPENDS ${SVF_EXTAPI_OUT_FILE})
+add_custom_target(gen_extapi_ir ALL DEPENDS "${EXTAPI_BIN_FILE}")
 
 # Install the bitcode file as well; install it to <prefix>/lib/extapi.bc
-install(FILES ${SVF_EXTAPI_OUT_FILE} DESTINATION ${SVF_INSTALL_LIBDIR})
+install(FILES "${EXTAPI_BIN_FILE}" DESTINATION "${SVF_INSTALL_LIBDIR}")
