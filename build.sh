ee6dd716f40e6f1f223acdfb9a8e31cf44305568
jumormt <jumormt@163.com>
2024-12-19 22:46:00 +1100

    use ubuntu latest

--- build.sh
+++ build.sh
@@ -250,10 +250,12 @@ BUILD_DIR="./${BUILD_TYPE}-build"
 rm -rf "${BUILD_DIR}"
 mkdir "${BUILD_DIR}"
 # If you need shared libs, turn BUILD_SHARED_LIBS on
-cmake -D CMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}" \
-    -DSVF_ENABLE_ASSERTIONS:BOOL=true            \
-    -DSVF_SANITIZE="${SVF_SANITIZER}"            \
-    -DBUILD_SHARED_LIBS=off                      \
+cmake -D CMAKE_BUILD_TYPE:STRING="${BUILD_TYPE}"   \
+    -DCMAKE_C_COMPILER="${LLVM_DIR}/bin/clang"     \
+    -DCMAKE_CXX_COMPILER="${LLVM_DIR}/bin/clang++" \
+    -DSVF_ENABLE_ASSERTIONS:BOOL=true              \
+    -DSVF_SANITIZE="${SVF_SANITIZER}"              \
+    -DBUILD_SHARED_LIBS=off                        \
     -S "${SVFHOME}" -B "${BUILD_DIR}"
 cmake --build "${BUILD_DIR}" -j ${jobs}
 
