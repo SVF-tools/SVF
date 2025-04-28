6f270db08153deeebc2d98c616ce82f8f35d5047
Jiawei Wang <jackwang834@gmail.com>
2025-04-28 23:32:20 +1000

    ECOOP 2025 AE Recursion implementation (#1694)
    * Improve recursion handling in abstract interpretation by leveraging interprocedural weak topological ordering
    
    * BugFix: Fix cycle handling bugs
    
    * Remove printf
    
    * BugFix: fix all ci failures
    
    * WIP: add option for selecting recursion handling mode
    
    * BugFix: fix double free
    
    * WIP: Implement widen-top
    
    * remove comments
    
    * 1. add recursion test to CI 2. fix some bugs
    
    * 1. add recursion test to CI 2. fix some bugs
    
    * refactor argv helper
    
    * recur-mode -> recur-handle
    
    * 1) recur-handle->handle-recur
    2) simplify helper info of handle-recur
    
    * set Widen-Narrow as default
    
    * Refine option description
    
    * Refine option description
    
    * Remove comments
    
    * Refine comments
    
    ---------
    
    Co-authored-by: joelyyoung <y2101831558@gmail.com>
    Co-authored-by: bjjwwang <bjjwwang@github.com>

--- svf/include/AE/Svfexe/AbstractInterpretation.h
+++ svf/include/AE/Svfexe/AbstractInterpretation.h
@@ -33,7 +33,8 @@
 #include "AE/Svfexe/AEDetector.h"
 #include "AE/Svfexe/AbsExtAPI.h"
 #include "Util/SVFBugReport.h"
-#include "WPA/Andersen.h"
+#include "Util/SVFStat.h"
+#include "Graphs/SCC.h"
 
 namespace SVF
 {
@@ -107,6 +108,29 @@ class AbstractInterpretation
 
 public:
     typedef SCCDetection<CallGraph*> CallGraphSCC;
+
+    /*
+     * For recursive test case
+     * int demo(int a) {
+        if (a >= 10000)
+            return a;
+            demo(a+1);
+        }
+
+        int main() {
+            int result = demo(0);
+        }
+     * if set TOP, result = [-oo, +oo] since the return value, and any stored object pointed by q at *q = p in recursive functions will be set to the top value.
+     * if set WIDEN_ONLY, result = [10000, +oo] since only widening is applied at the cycle head of recursive functions without narrowing.
+     * if set WIDEN_NARROW, result = [10000, 10000] since both widening and narrowing are applied at the cycle head of recursive functions.
+     * */
+    enum HandleRecur
+    {
+        TOP,
+        WIDEN_ONLY,
+        WIDEN_NARROW
+    };
+
     /// Constructor
     AbstractInterpretation();
 
@@ -135,7 +159,7 @@ private:
     /// Global ICFGNode is handled at the entry of the program,
     virtual void handleGlobalNode();
 
-    /// Mark recursive functions in the call graph
+    /// Compute IWTO for each function partition entry
     void initWTO();
 
     /**
@@ -251,7 +275,8 @@ private:
     AEStat* stat;
 
     std::vector<const CallICFGNode*> callSiteStack;
-    Map<const FunObjVar*, ICFGWTO*> funcToWTO;
+    Map<const FunObjVar*, const ICFGWTO*> funcToWTO;
+    Set<std::pair<const CallICFGNode*, NodeID>> nonRecursiveCallSites;
     Set<const FunObjVar*> recursiveFuns;
 
 
@@ -282,8 +307,10 @@ private:
     // helper functions in handleCallSite
     virtual bool isExtCall(const CallICFGNode* callNode);
     virtual void extCallPass(const CallICFGNode* callNode);
+    virtual bool isRecursiveFun(const FunObjVar* fun);
     virtual bool isRecursiveCall(const CallICFGNode* callNode);
-    virtual void recursiveCallPass(const CallICFGNode* callNode);
+    virtual void recursiveCallPass(const CallICFGNode *callNode);
+    virtual bool isRecursiveCallSite(const CallICFGNode* callNode, const FunObjVar *);
     virtual bool isDirectCall(const CallICFGNode* callNode);
     virtual void directCallFunPass(const CallICFGNode* callNode);
     virtual bool isIndirectCall(const CallICFGNode* callNode);
