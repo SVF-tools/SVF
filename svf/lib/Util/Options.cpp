20fc6e3f471354d63a60eba8fd640e0c8b96bb53
bjjwwang <bjjwwang@github.com>
2026-01-18 21:37:50 +1100

    TODO: implement use-sparse

--- svf/lib/Util/Options.cpp
+++ svf/lib/Util/Options.cpp
@@ -802,8 +802,9 @@ const OptionMap<u32_t> Options::HandleRecur(
     }
 }
 );
-const Option<bool> Options::UseSparseState(
-    "use-sparse", "Use sparse abstract state representation (experimental)", false);
+// TODO: to implement
+// const Option<bool> Options::UseSparseState(
+//     "use-sparse", "Use sparse abstract state representation (experimental)", false);
 const Option<u32_t> Options::Timeout(
     "timeout", "time out (seconds), set -1 (no timeout), default 14400s",14400);
 const Option<std::string> Options::OutputName(
